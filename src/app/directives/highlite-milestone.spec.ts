e0927c5778ca6ca224aeebeba2c746f09846e1b6
arshiamunir291 <arshiamunir786@gmail.com>
2026-02-11 01:29:43 -0500

    Setup the structure of the  app

---
+++ src/app/directives/highlite-milestone.spec.ts
@@ -0,0 +1,8 @@
+import { HighliteMilestone } from './highlite-milestone';
+
+describe('HighliteMilestone', () => {
+  it('should create an instance', () => {
+    const directive = new HighliteMilestone();
+    expect(directive).toBeTruthy();
+  });
+});
